a2393030ce4647fb75a7b3009b522f75dcd29fa5
DESKTOP-IA41EAO\CORBA <marti.corbalan@mataro.epiaedu.cat>
2020-11-17 20:59:22 +0100

    taula periodica

--- app/src/main/java/com/example/taulaperiodica/JocElements.java
+++ app/src/main/java/com/example/taulaperiodica/JocElements.java
@@ -3,6 +3,7 @@ package com.example.taulaperiodica;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class JocElements extends AppCompatActivity {
@@ -16,16 +17,28 @@ public class JocElements extends AppCompatActivity {
 
         enunciat = findViewById(R.id.textEnunciat);
 
+        preguntes();
 
+        RadioButton resp1;
+        RadioButton resp2;
+        RadioButton resp3;
+
+        resp1 = findViewById(R.id.resposta1);
+        resp2 = findViewById(R.id.resposta2);
+        resp3 = findViewById(R.id.resposta3);
+
+        resp1.setText(MainActivity.elements[(int) (Math.random()*116+0].getSimbol());
 
 
     }
 
     public void preguntes(){
+
         int respuesta = (int) (Math.random()*116+0);
         int Inrepetitivo = (int) (Math.random()*3+0);
-
         String enunciat2 = "sel·lecciona el símbol d'aquest element:" + MainActivity.elements[respuesta].getNom();
+
+        enunciat.setText(enunciat2);
 
 
 
